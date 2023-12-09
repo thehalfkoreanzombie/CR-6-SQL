2dc8a6d93c5dadd795b48f9dec9b29a8a55357b7
thehalfkoreanzombie <thehalfkoreanzombie@gmail.com>
2023-12-09 15:34:48 -0800

    organization

--- code_review.sql
+++ code_review.sql
@@ -1,4 +1,7 @@
 use lego;
+
+-- SELECTING VALUES
+
 --Select the name and part_num columns from the parts table. 
 SELECT name, part_num FROM parts;
 --Show only names that include the substring 'Hair', and order them with the part_num in descending order.
@@ -9,6 +12,8 @@ SELECT * FROM sets WHERE name LIKE '%Showdown%' AND year BETWEEN '1990' AND '201
 --Aliasing the part_categories table as p, show the id and name of the values in that table 
 --where the name includes the word 'Bricks'.
 SELECT p.id, p.name FROM part_categories AS p WHERE name LIKE '%Bricks%';
+
+-- SUBQUERIES
 
 --Select all the ids from the themes table with 'Pirates' or 'Star Wars' in the name. 
 SELECT id FROM themes WHERE name LIKE '%Pirates' OR '%Star Wars%';
@@ -23,6 +28,8 @@ SELECT id FROM inventories WHERE version > 1;
 --Limit the output to 10 rows.
 SELECT * FROM inventory_parts WHERE inventory_id IN (SELECT id FROM inventories WHERE version > 1) LIMIT 10;
 
+-- MANIPULATE VALUES IN SELECT 
+
 --Aliasing the sets table as 's' 
 --select the year and name of the values in that table that include the substring 'Batman'. 
 --Concatenate three exclamation marks to the end of each name, and make all the names upper case.
@@ -30,6 +37,8 @@ SELECT s.year, CONCAT(UPPER(s.name), '!!!') AS upper_name FROM sets AS s WHERE n
 
 --For every quantity value greater than 1 in the inventory_parts table, double the value of the quantity. Limit your output to 20 rows, and order it by quantity.
 SELECT quantity * 2 AS double_quantity FROM inventory_parts WHERE quantity > 1 ORDER BY quantity LIMIT 20;
+
+-- AGGREGATE FUNCTIONS 
 
 --Show the sum of all the parts for all the sets that came out since the year 2000.
 SELECT SUM(num_parts) AS sum_num_parts FROM sets WHERE year >= 2000;
@@ -39,6 +48,8 @@ SELECT AVG(num_parts) AS avg_num_parts FROM sets WHERE year >= 2000;
 
 --Using GROUP BY, show the average number of parts for each theme_id in sets.
 SELECT theme_id, AVG(num_parts) AS avg_num_parts FROM sets GROUP BY theme_id;
+
+-- JOIN
 
 --Using a free-form join (where the JOIN type isn't specified, equivalent to an inner join) 
 --join the parts and part_categories tables. 
